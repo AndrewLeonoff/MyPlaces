078722050c854612be2847dbaaf415be084c6157
Andrew Leonov <andrewleonov999@gmail.com>
2023-05-30 23:12:07 +0300

    Working with MapViewController

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Andrew Leonov on 30.05.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeVS() {
+        dismiss(animated: true)
+    }
+    
+}
