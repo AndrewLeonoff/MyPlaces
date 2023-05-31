615fa21a87d9a005137b178c5998e7df1074aa93
Andrew Leonov <andrewleonov999@gmail.com>
2023-05-31 21:55:58 +0300

    Center View In User Location

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController {
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
     let locationManager = CLLocationManager()
+    let regionInMeters = 10_000.00
 
     @IBOutlet var mapView: MKMapView!
     
@@ -22,6 +23,16 @@ class MapViewController: UIViewController {
         mapView.delegate = self
         setupPlacemark()
         checkLocationServices()
+    }
+    
+    @IBAction func centerViewInUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
     }
     
     @IBAction func closeVS() {
